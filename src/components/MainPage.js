69ab27a201a785bd9b7f0062dea7993e74f2c445
szaker1990 <szaker1990@github.com>
2020-08-12 14:12:54 +0200

    base styles

---
+++ src/components/MainPage.js
@@ -0,0 +1,42 @@
+import React from "react"
+import styled from 'styled-components'
+import background from "../assets/bg.png"
+import logo from "../assets/logo.png"
+import {PokeTable} from "./PokeTable";
+
+const Container = styled.div`
+height: 100vh;
+width: 100%;
+background: white;
+display: flex;
+`;
+const ColumnLeft = styled.div`
+width: 50%;
+margin: 20px;
+height: calc(100% - 40px);
+background: #ffffff url(${background}) no-repeat;
+background-size: cover
+`;
+const ColumnRight = styled.div`
+width: 50%;
+margin: 20px;
+height: calc(100% - 40px);
+background: yellow;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const MainPage = () => {
+    return(
+        <Container>
+            <ColumnLeft/>
+            <ColumnRight>
+                <img src={logo} alt={"Pokemon loogo"}/>
+                <PokeTable/>
+            </ColumnRight>
+
+        </Container>
+
+    )
+}
---
+++ src/components/PokeTable.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const PokeTable = () => {
+    return(
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>POKEMON</th>
+                <th>NAZWA</th>
+                <th>MIN.LVL</th>
+                <th>TYP</th>
+                <th>EWOLUCJA</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>001</td>
+                <td>img</td>
+                <td>BULBASAUR</td>
+                <td>001</td>
+                <td>TRAWA</td>
+                <td>IVOSAUR</td>
+            </tr>
+            </tbody>
+        </table>
+
+
+    )
+}
