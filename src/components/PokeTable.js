a3eb23d57df78240b86a806fd530cf1226456810
szaker1990 <szaker1990@github.com>
2020-08-12 17:51:00 +0200

    fetching data

--- src/components/PokeTable.js
+++ src/components/PokeTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from 'styled-components'
 import charmander from "../assets/Charmander_by_xous54.png"
 
@@ -8,9 +8,7 @@ width:100%;
 border:1px solid #C0C0C0;
 border-collapse:collapse;
 padding:5px
-;
-
-`
+;`
 const Tr = styled.tr`
 `
 const Th = styled.th`
@@ -33,11 +31,41 @@ max-height: 50px;
 margin: auto;
 `
 const Img = styled.img`
-height: 40px;
+height: 50px;
 width: 40px;
 padding: 5px 0;
 `
 export const PokeTable = () => {
+    const [pokemons, setPokemons] = useState([]);
+    const[pokeData,setPokeData] = useState([]);
+
+    const fetchPokemonData = (pokemon) => {
+        let url = pokemon.url
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setPokeData(prevState => [...prevState,data])
+            })
+    }
+    const getPokemons = (url) => {
+        fetch(url)
+            .then( resp => resp.json())
+            .then( data => {
+                setPokemons(data.results);
+                console.log(data)
+                let pokeData = data.results.map((pokemon) => {
+                    fetchPokemonData(pokemon)
+                })
+            })
+            .catch( err => console.log(err));
+    }
+
+    useEffect(()=>{
+        getPokemons('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+    }, []);
+
+
     return(
         <Table>
             <thead>
@@ -51,86 +79,16 @@ export const PokeTable = () => {
             </Tr>
             </thead>
             <tbody>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
-            <Tr>
-                <Td>001</Td>
-                <Td><Img src={charmander} alt={charmander}/></Td>
-                <Td>BULBASAUR</Td>
-                <Td>001</Td>
-                <Td>TRAWA</Td>
-                <Td>IVOSAUR</Td>
-            </Tr>
+            {pokeData.sort((a,b) => a -b).map(pokemon => (
+                <Tr key={pokemon.name}>
+                    <Td>{pokemon.id}</Td>
+                    <Td><Img src={pokemon.sprites.front_default} alt={"pokemon"}/></Td>
+                    <Td>{pokemon.name}</Td>
+                    <Td>001</Td>
+                    <Td>{pokemon.types[0].type.name}</Td>
+                    <Td>IVOSAUR</Td>
+                </Tr>
+            ))}
             </tbody>
         </Table>
 
